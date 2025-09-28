a22cf2b3603a98db92d1797a5b4132709b707793
frank <nhat.thb@gmail.com>
2025-09-28 17:26:03 +0700

    add barcode scanning for demo purpose

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,7 +52,7 @@ android {
 }
 
 dependencies {
-
+    implementation("com.google.mlkit:barcode-scanning:17.0.3")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.2")
